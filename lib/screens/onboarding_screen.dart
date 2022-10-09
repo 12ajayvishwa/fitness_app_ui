fd5c3fb651006a982915a71ab60e0e9f600332b5
12ajayvishwa <ajayvish21099@gmail.com>
2022-10-09 16:16:18 +0530

    animated curve

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fitness_ui/utils/colors.dart';
+import 'package:fitness_ui/widgets/animated_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -9,6 +12,12 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final pageController = PageController(initialPage: 0);
+
+  void nextPage() {
+    pageController.nextPage(
+        duration: const Duration(milliseconds: 20), curve: Curves.ease);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +28,18 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             controller: pageController,
             physics: NeverScrollableScrollPhysics(),
             children: [
-              Slide(text:"slide 1", onNext: () {  },),
-              Slide(text:"slide 2", onNext: () {  },),
-              Slide(text:"slide 3", onNext: () {  },),
+              Slide(
+                text: "slide 1",
+                onNext: nextPage,
+              ),
+              Slide(
+                text: "slide 2",
+                onNext: nextPage,
+              ),
+              Slide(
+                text: "slide 3",
+                onNext: nextPage,
+              ),
             ],
           ),
         )));
@@ -32,7 +50,9 @@ class Slide extends StatelessWidget {
   final String text;
   final VoidCallback onNext;
   const Slide({
-    Key? key, required this.text, required this.onNext,
+    Key? key,
+    required this.text,
+    required this.onNext,
   }) : super(key: key);
 
   @override
@@ -40,8 +60,54 @@ class Slide extends StatelessWidget {
     return Container(
       child: Center(
           child: Column(
-        children: [Text(text), InkWell(onTap: onNext, child: Container())],
+        children: [
+          
+          Text(text),
+          InkWell(onTap: onNext, child: Container()),
+          ProgressButton(
+            onNext: onNext,
+          )
+        ],
       )),
     );
+  }
+}
+
+class ProgressButton extends StatelessWidget {
+  final VoidCallback onNext;
+  const ProgressButton({Key? key, required this.onNext}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 75,
+        width: 75,
+        child: Stack(
+          children: [
+            AnimatedIndicaor(
+            duration:const Duration(seconds: 10),
+            size:75,
+            callback:onNext
+          ),
+            Center(
+              child: GestureDetector(
+                  child: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [kBrandColor1, kBrandColor2])),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          "assets/icons/light/Arrow - Right 2.svg",
+                          color: Colors.white,
+                        ),
+                      ))),
+            )
+          ],
+        ));
   }
 }
