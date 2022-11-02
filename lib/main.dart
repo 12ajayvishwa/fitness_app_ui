f8e73412ee5154b23c78009f794c276b5d793d5b
Ajay vishwa <ajayvish21099@gmail.com>
2022-11-02 13:04:40 +0530

    change in main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fitness',
       onGenerateRoute: CustomRouter.generateRoute,
       initialRoute: welcomeScreenRoute,
     );
